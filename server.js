05d04ce36d7ee8a0374cb16e97bc1fd75a27abc9
SandeepBaskaran <sandeepbaskaran98@gmail.com>
2018-02-24 12:02:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.get('/404', function (req, res){
     res.send('ERROR:404 not found');
 })
 
+app.get('/profile', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'profile.html'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
